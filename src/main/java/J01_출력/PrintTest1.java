fd93a7d95f8cfffdf88c7211e94ad0c3cd7b576b
WaraGe <zpfhfh774@naver.com>
2022-10-25 21:10:53 +0900

    chapter1 - 출력

---
+++ src/main/java/J01_출력/PrintTest1.java
@@ -0,0 +1,23 @@
+package J01_출력;
+
+public class PrintTest1 {
+    public static void main(String[] args) {
+        //main, psvm  =  main문 자동완성
+        /*
+        여러줄 주석
+         */
+
+        /**
+         * 클래스 또는 메소드의 역할을 명시 또는 설명하기 위한 주석
+         */
+
+        // 문자열 출력
+        System.out.print("이름:");
+        System.out.print("이성욱 \n");
+        System.out.print("주소: ");
+        System.out.println("부산 동래구 사직동");
+
+        boolean IsUsernameCheckFlag = true;
+        System.out.println(IsUsernameCheckFlag);
+    }
+}
