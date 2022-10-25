a0786902a19e4db40150de398d8dfec4103d5e50
WaraGe <zpfhfh774@naver.com>
2022-10-25 21:11:28 +0900

    chapter2 - 변수와 상수

---
+++ src/main/java/j02_변수상수/Variable.java
@@ -0,0 +1,71 @@
+package j02_변수상수;
+
+/* 변수 */
+public class Variable {
+    public static void main(String[] args) {
+
+        // 논리자료형 변수
+       /* boolean checkFlag = false;
+        System.out.println(checkFlag);
+        checkFlag = true;
+        System.out.println(checkFlag);*/
+
+        // 문자자료형 변수
+        // 라인복사 Ctrl + d
+        /*char name1 = '이';
+        char name2 = '성';
+        char name3 = '욱';
+        System.out.println(name1 + name2 + name3);
+        System.out.println("" + name1 + name2 + name3);
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println('가' + 0);*/
+
+        // 문자열 자료형 변수
+        /*String name = "이성욱";
+        String name2 = " 윤도영";
+        String name3  = name+name2;
+        System.out.println(name);
+        System.out.println(name3);*/
+
+        // 정수 자료형 변수
+        /*int width = 100;
+        int width2 = 200;
+
+        String width3 = "300";
+        String width4 = "400";
+
+        int widthResult = width + width2;
+        String widthResult2 = width3 + width4;
+
+        System.out.println(widthResult);
+        System.out.println(widthResult2);*/
+
+        /*long time = System.currentTimeMillis();
+        System.out.println(time);
+
+        long time2 = 1666178982748L;
+        System.out.println(time2);
+
+        // 실수자료형 변수
+        double pi = 3.1415926539999599;
+        System.out.println(pi);
+
+        float a = 3.14F;
+        System.out.println(a);*/
+
+        /*final double PI = 3.14;
+        System.out.println(PI);
+
+        *//*PI = 3.15;*//*
+        System.out.println(PI);*/
+
+        final String FILE_PATH = "C:/Users/";
+
+        // FILE_PATH = "D:/Users/aaa";
+        System.out.println(FILE_PATH);
+
+
+    }
+}
